46f3f39da9c0ff84acfefe8ba638f4318aefb107
Nayan Kyada <kayadanayan@gmail.com>
2023-06-01 10:27:54 +0530

    feat: conditional rendering of div

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ function showSubscribeDiv() {
 function showUnSubscribeDiv() {
   document.getElementById("subscribeChoice").style.display = "none";
 }
+
+function validateEmailAddress(emailString) {
+  let atSymbol = emailString.indexOf("@");
+  let dot = emailString.indexOf(".");
+
+  if (atSymbol < 1) return false;
+  if (dot <= atSymbol + 2) return false;
+  if (dot === emailString.length - 1) return false;
+  return true;
+}
+
+function nextButtonAction() {
+  let emailInput = document.getElementById("emailInput");
+  let radioButtons = document.getElementsByName("subscriptionRadios");
+  let emailResult = validateEmailAddress(emailInput.value);
+
+  if (!emailResult) {
+    document.getElementById("emailAddressErr").style.display = "flex";
+    document.getElementById("pageErrors").style.display = "flex";
+  } else {
+    emailInput.style.display = "none";
+    document.getElementById("emailAddressErr").style.display = "none";
+    document.getElementById("pageErrors").style.display = "none";
+    document.getElementById("emailValue").innerHTML = emailInput.value;
+  }
+
+  for (i = 0; i < radioButtons.length; i++) {
+    if (!radioButtons[i].checked) {
+      document.getElementById("radioError").style.display = "flex";
+      document.getElementById("pageErrors").style.display = "flex";
+    } else if (radioButtons[i].checked) {
+      document.getElementById("radioError").style.display = "none";
+      document.getElementById("pageErrors").style.display = "none";
+    }
+  }
+}
